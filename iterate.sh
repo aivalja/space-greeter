b8cc554db552ce05621a6da8b40771a12b5b43fe
Anssi Valjakka <anssi.valjakka@gmail.com>
2020-09-06 12:08:38 +0300

    Run more tests using dup1.txt

--- iterate.sh
+++ iterate.sh
@@ -2,7 +2,7 @@
 
 #./detect.o 0 --cascade=lbpcascades/lbpcascade_frontalface.xml --scale=1 --test --silent --radius=1 --neighbours=3
 
-for radius in 5 7 10 14 20 30 50 
+for radius in 1 2 3 4 5 7 10 14 20 30 
 do
     for neighbours in 1 2 3 5 7 8 10
     do
