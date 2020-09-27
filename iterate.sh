06d9b6ffac4bca9e3986424ece154aad27b479ba
Anssi Valjakka <anssi.valjakka@gmail.com>
2020-09-27 17:06:44 +0300

    Rerun tests with all faces dup1

--- iterate.sh
+++ iterate.sh
@@ -2,9 +2,9 @@
 
 #./detect.o 0 --cascade=lbpcascades/lbpcascade_frontalface.xml --scale=1 --test --silent --radius=1 --neighbours=3
 
-for radius in 1 2 3 4 5 6 7 8 9 10 14 20 30 
+for radius in 1 2 3 5 7 10 14 20 30 
 do
-    for neighbours in 1 2 3 4 5 6 7 8 9 10
+    for neighbours in 1 2 3 5 7 8 10
     do
         # $ "run --radius=$radius --neighbours=$neighbours"
         ./detect.o 0 --cascade=lbpcascades/lbpcascade_frontalface.xml --scale=1 --test --silent --radius=$radius --neighbours=$neighbours
