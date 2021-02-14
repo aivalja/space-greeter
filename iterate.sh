acb10605b714c86ce053ea272d1f89d8fd020ae0
Anssi Valjakka <anssi.valjakka@gmail.com>
2021-02-14 03:00:49 +0200

    Ran dup1-tests

--- iterate.sh
+++ iterate.sh
@@ -1,7 +1,8 @@
 #/bin/bash
 
 #./detect.o 0 --cascade=lbpcascades/lbpcascade_frontalface.xml --scale=1 --test --silent --radius=1 --neighbours=3
-radius_values=(1 2 3 4 5 6 7 8 10 14 18 22 24 28 30) #1-5 done already
+#radius_values=(1 2 3 4 5 6 7 8 10 14 18 22 24 28 30) #1-22 done already
+radius_values=(24 28 30) #1-22 done already
 neighbours_values=( 1 2 3 4 5 6 7 8 9 10 )
 total=$((${#radius_values[@]}*${#neighbours_values[@]}))
 start=`date +%s`
@@ -11,10 +12,14 @@ do
     for neighbours in "${neighbours_values[@]}"
     do
         loops=$(($loops+1))
-        echo "Round $loops out of $total"
+        echo "Round ${loops}/${total}"
         # $ "run --radius=$radius --neighbours=$neighbours"
         ./detect.o 0 --cascade=lbpcascades/lbpcascade_frontalface.xml --scale=1 --test --silent --radius=$radius --neighbours=$neighbours
         current=`date +%s`
-	echo "Time left: $((($current - $start)/loops*(total-loops)))s"
+	left_total_s=$((($current - $start)/loops*(total-loops)))
+	left_h=$(($left_total_s/3600))
+	left_min=$((($left_total_s-$left_h*3600)/60))
+	left_s=$((($left_total_s-$left_h*3600-$left_min*60)))
+	echo "Time left ${left_h}h ${left_min}min ${left_s}s"
     done
 done
